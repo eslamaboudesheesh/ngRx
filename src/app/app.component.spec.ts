75234fc888b8b1a6c6879378c5e7ec34f203f86e
EslamAboudesheesh <t-emhussein@EFG-HERMES.com>
2020-01-21 15:07:15 +0200

    add  unit test for component

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,31 +1,66 @@
-import { TestBed, async } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { Store } from '@ngrx/store';
 import { AppComponent } from './app.component';
-
 describe('AppComponent', () => {
-  beforeEach(async(() => {
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+  beforeEach(() => {
+    const storeStub = () => ({
+      subscribe: function0 => ({}),
+      dispatch: arg => ({})
+    });
     TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
-      ],
-    }).compileComponents();
-  }));
-
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
+      schemas: [NO_ERRORS_SCHEMA],
+      declarations: [AppComponent],
+      providers: [{ provide: Store, useFactory: storeStub }]
+    });
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
   });
-
-  it(`should have as title 'ngRxApp'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('ngRxApp');
+  it('can load instance', () => {
+    expect(component).toBeTruthy();
   });
-
-  it('should render title in a h1 tag', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to ngRxApp!');
+  it('count defaults to: 0', () => {
+    expect(component.count).toEqual(0);
+  });
+  describe('ngOnInit', () => {
+    it('makes expected calls', () => {
+      spyOn(component, 'loadUsers').and.callThrough();
+      component.ngOnInit();
+      expect(component.loadUsers).toHaveBeenCalled();
+    });
+  });
+  describe('increase', () => {
+    it('makes expected calls', () => {
+      const storeStub:any = fixture.debugElement.injector.get(Store);
+      spyOn(storeStub, 'dispatch').and.callThrough();
+      component.increase();
+      expect(storeStub.dispatch).toHaveBeenCalled();
+    });
+  });
+  describe('decrease', () => {
+    it('makes expected calls', () => {
+      const storeStub:any = fixture.debugElement.injector.get(Store);
+      spyOn(storeStub, 'dispatch').and.callThrough();
+      component.decrease();
+      expect(storeStub.dispatch).toHaveBeenCalled();
+    });
+  });
+  describe('load', () => {
+    it('makes expected calls', () => {
+      const storeStub: any = fixture.debugElement.injector.get(Store);
+      spyOn(storeStub, 'dispatch').and.callThrough();
+      component.load();
+      expect(storeStub.dispatch).toHaveBeenCalled();
+    });
+  });
+  describe('loadUsers', () => {
+    it('makes expected calls', () => {
+      const storeStub: any = fixture.debugElement.injector.get(Store);
+      spyOn(storeStub, 'dispatch').and.callThrough();
+      component.loadUsers();
+      expect(storeStub.dispatch).toHaveBeenCalled();
+    });
   });
 });
